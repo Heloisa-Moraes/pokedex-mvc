780f3ab40c79c5013a74376433b2770da2dd9a64
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-08-21 22:22:19 -0300

    ultima atualizacao 21-08

--- Pokedex/Controllers/AccountController.cs
+++ Pokedex/Controllers/AccountController.cs
@@ -1,32 +1,106 @@
+using System.Net.Mail;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
-  
-    public class AccountController : Controller
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager
+        )
     {
-        private readonly ILogger<AccountController> _logger;
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
 
-        public AccountController(ILogger<AccountController> logger)
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM loginVM = new()
         {
-            _logger = logger;
-        }
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(loginVM);
+    }
 
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login (LoginVM login)
+    {
+        if (ModelState.IsValid)
         {
-            LoginVM loginVM = new() 
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
             {
-                UrlRetorno= returnUrl ?? Url.Content("~/")
-            };
-            return View(loginVM);
-        }
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar,lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário{login.Email}acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut) 
+            {
+                _logger.LogWarning($"!Usuário {login.Email} foi bloqueado");
+                return RedirectToAction("Lockout");
+
+            }
+            
+            ModelState.AddModelError(string.Empty, "usuário e/ou senha inválidos!!");
+        }
+        
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez o logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index" , "Home");
+    }
+
+    public IActionResult Lockout()
+    {
+        return View();
+    }
+    private bool IsValidEmail(string email)
+    {
+        try
         {
-            return View("Error!");
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch (System.Exception)
+        {
+            
+            return false;
         }
     }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
 
--- Pokedex/ViewModels/LogimVM.cs
+++ Pokedex/ViewModels/LogimVM.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Pokedex.ViewModels;
 
-    public class LogimVM
+    public class LoginVM
     {
         [Display(Name = "Email ou Nome de Usuário", Prompt ="Email ou Nome de Usuário")]
         [Required(ErrorMessage = "Informe seu email ou nome de usuário")]
         public string Email { get; set; }
+        
         [Display(Name = "Senha de Acesso", Prompt ="Senha e Acesso")]
         [Required(ErrorMessage = "Informe sua senha de acesso")]
 
